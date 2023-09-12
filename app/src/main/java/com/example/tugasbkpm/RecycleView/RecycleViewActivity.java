e6bc78a05b79cfdde86ff23be24938a185113867
SiTelur <70305841+SiTelur@users.noreply.github.com>
2023-09-12 19:53:00 +0700

    Recycle View

---
+++ app/src/main/java/com/example/tugasbkpm/RecycleView/RecycleViewActivity.java
@@ -0,0 +1,45 @@
+package com.example.tugasbkpm.RecycleView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.example.tugasbkpm.LinearLayout;
+import com.example.tugasbkpm.R;
+import com.example.tugasbkpm.model.Mahasiswa;
+import com.example.tugasbkpm.model.MahasiswaAdapter;
+
+import java.util.ArrayList;
+
+public class RecycleViewActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private MahasiswaAdapter adapter;
+    private ArrayList<Mahasiswa> MahasiswaArrayList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycle_view);
+addData();
+        recyclerView = findViewById(R.id.rv);
+
+        adapter = new MahasiswaAdapter(MahasiswaArrayList);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(RecycleViewActivity.this);
+
+        recyclerView.setLayoutManager(layoutManager);
+
+        recyclerView.setAdapter(adapter);
+
+    }
+
+    private void addData(){
+        MahasiswaArrayList = new ArrayList<>();
+        MahasiswaArrayList.add(new Mahasiswa("E41221566","Mochammad Lury Choirul Rizky","0895322195063"));
+        MahasiswaArrayList.add(new Mahasiswa("E42344322","Deli Kurniawati","085895680106"));
+    }
+
+
+}
--- app/src/main/java/com/example/tugasbkpm/model/MahasiswaAdapter.java
+++ app/src/main/java/com/example/tugasbkpm/model/MahasiswaAdapter.java
@@ -1,7 +1,55 @@
 package com.example.tugasbkpm.model;
 
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class MahasiswaAdapter{
+import com.example.tugasbkpm.R;
 
+import java.util.ArrayList;
+
+public class MahasiswaAdapter extends RecyclerView.Adapter<MahasiswaAdapter.MahasiswaViewHolder>{
+
+    private ArrayList<Mahasiswa> dataList;
+
+    public MahasiswaAdapter(ArrayList<Mahasiswa>dataList){
+        this.dataList = dataList;
+    }
+
+
+    @NonNull
+    @Override
+    public MahasiswaViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.list_item, parent, false);
+        return  new MahasiswaViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MahasiswaViewHolder holder, int position) {
+        holder.txtNama.setText(dataList.get(position).getNama());
+        holder.txtNIM.setText(dataList.get(position).getNim());
+        holder.txtNOHP.setText(dataList.get(position).getNohp());
+    }
+
+    @Override
+    public int getItemCount() {
+        return (dataList != null)? dataList.size(): 0 ;
+    }
+
+    public class MahasiswaViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView txtNama,txtNIM,txtNOHP;
+        public MahasiswaViewHolder(@NonNull View itemView) {
+            super(itemView);
+            txtNama = itemView.findViewById(R.id.txtNama);
+            txtNIM = itemView.findViewById(R.id.txtNIM);
+            txtNOHP = itemView.findViewById(R.id.txtnohp);
+        }
+    }
 }
